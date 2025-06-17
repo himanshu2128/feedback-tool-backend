e6e3b29763c689834b1a46aff89fea0ce1ebbb24
Himanshu Ranjan <youremail@example.com>
2025-06-17 13:13:23 +0530

    Added feedback route and server updates

---
+++ models/Feedback.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const feedbackSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+  },
+  submittedAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+module.exports = mongoose.model('Feedback', feedbackSchema);
